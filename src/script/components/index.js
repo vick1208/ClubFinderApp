8c0147a07fb55d9bb458552298d0dc390ba9e26b
Vicky AS <vickyhuang808@gmail.com>
2024-07-30 23:54:45 +0700

    ubah component

--- src/script/components/index.js
+++ src/script/components/index.js
@@ -5,7 +5,3 @@ import './club-list.js';
 import './club-item.js';
 
 import './search-bar.js';
-// import './section-with-title.js';
-
-// import './query-waiting.js';
-// import './search-loading.js';
