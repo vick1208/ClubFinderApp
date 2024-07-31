814f62f309d6dd8ca2ed866cd17e69cef9e9c1c3
Vicky AS <vickyhuang808@gmail.com>
2024-07-31 21:35:31 +0700

    new plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname,'src/app.js'),
-    output:{
-        path: path.join(__dirname,'dist'),
+    entry: path.join(__dirname, 'src/app.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: /\.css$/,
-                use:['style-loader','css-loader'],
+                use: ['style-loader', 'css-loader'],
             },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname,'index.html'),
+            template: path.join(__dirname, 'index.html'),
             filename: 'index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, 'src/public'),
+                    to: path.join(__dirname, 'dist'),
+                },
+            ],
         }),
     ]
 };
